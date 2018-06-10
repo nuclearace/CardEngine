e41e1f089cad2df3b9e40eb55f5cdf8f30101bf3
Erik Little <nuclear.ace@gmail.com>
2018-06-10 13:57:03 -0400

    finish play

--- Public/js/builders.jsx
+++ Public/js/builders.jsx
@@ -18,6 +18,11 @@ export class BuildersGame extends Component {
         this.state.turn = null;
         this.state.messages = [];
         this.state.hand = [];
+        this.state.cardsToPlay = [];
+
+        // Bind this
+        this.playCards = this.playCards.bind(this);
+        this.playCard = this.playCard.bind(this);
     }
 
     parseMessage(messageObject) {
@@ -44,10 +49,37 @@ export class BuildersGame extends Component {
         }
     }
 
+    playCard(cardNum) {
+        this.setState(prevState => {
+            const newPlay = prevState.cardsToPlay.slice();
+
+            newPlay.push(cardNum);
+
+            return {
+                turn: 'play',
+                hand: prevState.hand,
+                cardsToPlay: newPlay
+            }
+        })
+    }
+
+    playCards() {
+        this.ws.send(JSON.stringify({
+            "play": this.state.cardsToPlay
+        }))
+    }
+
     render() {
         switch (this.state.turn) {
         case 'play':
-            return <PlayerHand hand={this.state.hand} />;
+            return (
+                <div>
+                    <PlayerHand hand={this.state.hand}
+                                onPlay={this.playCard}
+                                hide={this.state.cardsToPlay}/>
+                    <button onClick={this.playCards}>Play selected cards</button>
+                </div>
+            );
         default:
             return <h2>'Waiting!'</h2>;
         }
@@ -56,13 +88,22 @@ export class BuildersGame extends Component {
 
 class PlayerHand extends Component {
     render() {
+        this.onPlay = this.props.onPlay;
+
         return (
             <ul>
                 {this.props.hand.map((card, i) => {
-                    return <PlayerCard key={i} card={card}/>;
+                    return <PlayerCard key={i}
+                                       card={card}
+                                       onPlay={() => this.playCard(i)}
+                                       hide={this.props.hide.indexOf(i) !== -1} />;
                 })}
             </ul>
         )
+    }
+
+    playCard(cardNum) {
+        this.onPlay(cardNum);
     }
 }
 
@@ -73,8 +114,7 @@ class PlayerCard extends Component {
         return (
             <li>
                 <span>
-                    {/*TODO figure out how to actually play this*/}
-                    <button>Play</button>
+                    <button onClick={this.props.onPlay} disabled={this.props.hide}>Play</button>
                     Type {type}
                 </span>
             </li>
